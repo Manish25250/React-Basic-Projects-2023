bd99113facd1940d53a21ca333ff6b9fd1f2a55a
MANISH <manish25250@gmail.com>
2023-06-10 13:13:16 +0530

    05 navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./04-grocery-bud/App";
+import App from "./05-navbar/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
