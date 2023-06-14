b26e95188f8b911470bac0fd761143ae14f99bf2
MANISH <manish25250@gmail.com>
2023-06-14 12:45:59 +0530

    06 sidebar and modal

---
+++ src/06-sidebar-modal/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
+
+const Home = () => {
+  // const data = useContext(AppContext);
+  // const data = useGlobalContext();
+  // console.log(data);
+  const { openModal, openSidebar } = useGlobalContext();
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
