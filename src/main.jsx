54d1e54f3eee9df1059a0e434eb459cd2c0f3ab3
MANISH <manish25250@gmail.com>
2023-06-15 13:20:21 +0530

    07 stripe submenu

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./06-sidebar-modal/App";
-import { AppProvider } from "./06-sidebar-modal/context";
+import App from "./07-stripe-submenus/App";
+//import { AppProvider } from "./06-sidebar-modal/context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
